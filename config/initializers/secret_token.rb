6cd54fc5708d1e54fe37e1267eb1fc2741fdc5f6
odradeck <odradeck@gmail.com>
2012-11-06 13:12:10 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lulusum::Application.config.secret_token = 'b578a0458484b825f593076b2aeb9c07aac45d015d9a30d13077130943bf3facef09360a72f1c45387ce0b7c757963b0fb8a97bf6767f83ec583283619219095'
